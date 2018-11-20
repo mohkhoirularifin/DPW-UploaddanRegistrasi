8ccc77f34054de86f2e1a4d5cc0630f74ee5faee
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-11-20 20:23:53 +0700

    menambahkan fungsi rehistrasi

--- functions.php
+++ functions.php
@@ -154,4 +154,42 @@ function upload()
     // kita return nama file nya agar dapat masuk ke $gambar=$upload() pada function tambah
     return $namafilebaru;
 }
+
+function registrasi($data)
+{
+    global $conn;
+
+    $username=strtolower(stripcslashes($data['username']));
+
+    $password=mysqli_real_escape_string($conn, $data['password']);
+    $password2=mysqli_real_escape_string($conn, $data['password2']);
+
+    $result=mysqli_query($conn, "SELECT username FROM users WHERE username='$username'");
+
+    if (mysqli_fetch_assoc($result)) 
+    {
+        echo "
+            <script>
+                alert('username sudah ada');
+            </script>
+        ";
+        return false;
+    }
+
+    if ($password!==$password2) 
+    {
+        echo "
+            <script>
+                alert('password anda tidak sama');
+            </script>
+        ";
+        return false;
+    }
+
+    $password=password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO users VALUES ('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
 ?>
